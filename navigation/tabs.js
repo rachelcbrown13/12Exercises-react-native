ff3f59a2b15031e8dc944a177d88967cf0b2d132
RBrown <rbrown@us.ci.org>
2024-01-22 06:49:17 -0800

    images and boxes added to exercise

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,23 +1,50 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-
 import HomeScreen from '../screens/HomeScreen';
-import Exercise1Screen from '../screens/Exercise1Screen';
+import Exercise1Screen from '../screens/CurrentExercise';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHome, fa5 } from '@fortawesome/free-solid-svg-icons'
 
-const Tab = createBottomTabNavigator() 
-    
-function MyTabs () {
+
+
+const Tab = createBottomTabNavigator()
+
+function MyTabs() {
     return (
         <NavigationContainer>
-        <Tab.Navigator initialRouteName='Home'
-        screenOptions={{
-            tabBarActiveTintColor: '#acb456',
-            tabBarStyle: { position: 'absolute', backgroundColor: '#123456' },
-          }}>
-          <Tab.Screen name='Home' component={HomeScreen} />
-          <Tab.Screen name='Exercise1' component={Exercise1Screen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+            <Tab.Navigator initialRouteName='Home'
+                screenOptions={{
+                    tabBarActiveTintColor: 'black',
+                    tabBarActiveBackgroundColor: '#acb456',
+                    tabBarStyle: { position: 'absolute', backgroundColor: '#123456' },
+
+                }}
+            >
+                <Tab.Screen
+                    name='Home'
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: () => {
+                            return (
+                                <FontAwesomeIcon icon={faHome} size={24} 
+                                color={"#8e8e93"} />
+                            )
+                        }
+                    }}
+                />
+                <Tab.Screen
+                    name='Exercise 5'
+                    component={Exercise1Screen}
+                    options={{
+                        tabBarIcon: () => {
+                            return (
+                                <FontAwesomeIcon icon={fa5} color='white'/>
+                            )
+                        }
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 };
 
---
+++ screens/CurrentExercise.tsx
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+function CurrentExerciseScreen() {
+    return (
+        <View style={styles.wholeSection}>
+            <View style={styles.ejSection}>
+                <Image source={require('../assets/EJonPlane.jpeg')} style={styles.image}/>
+                <Text style={styles.text}>EJ</Text>
+            </View>
+            <View style={styles.apollosSection}>
+                <Image source={require('../assets/Apollos.jpeg')} style={styles.image}/>
+                <Text style={styles.text}>Apollos</Text>
+            </View>
+            <View style={styles.imriSection}>
+                <Image source={require('../assets/Imri.jpeg')} style={styles.image}/>
+                <Text style={styles.text}>Imri</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wholeSection: {
+        flexDirection:'row',
+        justifyContent: 'space-evenly',
+
+    },
+    ejSection: {
+        backgroundColor: 'red',
+        justifyContent: 'center',
+    },
+    apollosSection: {
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+    },
+    imriSection: {
+        backgroundColor: 'purple',
+        justifyContent: 'center',
+    },
+    text: {
+        color:'white', 
+        textAlign:'center',
+        fontSize: 24
+    },
+    image: {
+        width: 100,
+        height: 100,
+    }
+})
+
+
+export default CurrentExerciseScreen;
