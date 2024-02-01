d2f884e93c6566e2600f8d0f47e5760b533eff73
RBrown <rbrown@us.ci.org>
2024-02-01 14:11:49 -0800

    Maisie completed exercise 8 - Flatlist

--- screens/CurrentExercise.tsx
+++ screens/CurrentExercise.tsx
@@ -1,38 +1,78 @@
-import { View, TextInput, Text, Button, Alert, StyleSheet } from 'react-native';
-import { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {View, Text, FlatList, ScrollView, StyleSheet} from 'react-native'
 
-
-
-function CurrentExercise() {
-    const [name, setName] = useState('');
-
-    function alert() {
-        return Alert.alert(name);
-    }
-
-    return (
-        <View>
-            <Text>What is your name?</Text>
-            <SafeAreaView style={styles.nameForm}>
-                <TextInput style={styles.input} placeholder='First Name' onChangeText={setName}/>
-                <TextInput style={styles.input} placeholder='Last Name' onChangeText={setName}/>
-            </SafeAreaView>
-            <Button title='Say Hello!' onPress={alert}></Button>
-        </View>
-    )
+type objectData = {
+    id: string,
+    name: string,  
 }
 
-const styles = StyleSheet.create({
-    nameForm: {
-        flexDirection: 'row'
+type ItemProps = {
+    name: string;
+}
+
+const DATA: objectData[] = [
+    {
+        id: '1',
+        name:'Rachel'
     },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-      }
+    {
+        id: '2',
+        name:'Maisie'
+    },
+    {
+        id: '3',
+        name:'Big Mike T'
+    },
+    {
+        id: '4',
+        name:'Imri Girl'
+    },
+    {
+        id: '5',
+        name:'Josh'
+    },
+    {
+        id: '6',
+        name:'Bethany'
+    },
+    {
+        id: '7',
+        name:'Kurtis'
+    }
+]
+
+const Item = ({name}: ItemProps) => (
+    <View style={styles.box}>
+        <Text>{name}</Text>
+    </View >
+)
+function CurrentExercise() {
+    const answer = [];
+    
+    return (
+        <FlatList
+            data={DATA}
+            renderItem={({item}) => <Item name={item.name}/>}
+            keyExtractor={item => item.id}
+        />
+           
+    )
+}
+const styles = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 100,
+    },
+    scrollView: {
+        padding: 8,
+        flex: 1,
+        alignItems: 'center'
+    },
+    box: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#42cab6',
+        margin: 8
+    }
 })
 
 export default CurrentExercise;
---
+++ screens/ExerciseSeven.tsx
@@ -0,0 +1,38 @@
+import { View, TextInput, Text, Button, Alert, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
+function ExerciseSeven() {
+    const [name, setName] = useState('');
+
+    function alert() {
+        return Alert.alert(name);
+    }
+
+    return (
+        <View>
+            <Text>What is your name?</Text>
+            <SafeAreaView style={styles.nameForm}>
+                <TextInput style={styles.input} placeholder='First Name' onChangeText={setName}/>
+                <TextInput style={styles.input} placeholder='Last Name' onChangeText={setName}/>
+            </SafeAreaView>
+            <Button title='Say Hello!' onPress={alert}></Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    nameForm: {
+        flexDirection: 'row'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      }
+})
+
+export default ExerciseSeven;
